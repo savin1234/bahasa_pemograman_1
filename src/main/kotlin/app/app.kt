70b0d4998a341477d58372519e07d50e359f1442
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 13:47:30 +0700

    first commit

---
+++ src/main/kotlin/app/app.kt
@@ -0,0 +1,8 @@
+package app
+
+import data.person
+fun main() {
+    var avin = person()
+
+   println ("avin")
+}
---
+++ src/main/kotlin/latihan.kt
@@ -0,0 +1,12 @@
+fun main (){
+      println ("WKWKWKWKWKWKWKWKWWKKW")
+      println ("AVIN")
+
+      print("Pekerjaan : ")
+      val pekerjaan = readLine()
+
+      print("Alamat : ")
+      val alamat = readLine()
+
+      println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+}
